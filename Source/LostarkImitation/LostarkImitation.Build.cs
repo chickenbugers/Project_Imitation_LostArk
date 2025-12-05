cf616fa7fa3472f7601ce131fc41008a815803e0
CJS <gksdidxornjs@naver.com>
2025-12-06 01:51:40 +0900

    Init Unreal Setting

---
+++ Source/LostarkImitation/LostarkImitation.Build.cs
@@ -0,0 +1,46 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class LostarkImitation : ModuleRules
+{
+	public LostarkImitation(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"NavigationSystem",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"Niagara",
+			"UMG",
+			"Slate"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"LostarkImitation",
+			"LostarkImitation/Variant_Strategy",
+			"LostarkImitation/Variant_Strategy/UI",
+			"LostarkImitation/Variant_TwinStick",
+			"LostarkImitation/Variant_TwinStick/AI",
+			"LostarkImitation/Variant_TwinStick/Gameplay",
+			"LostarkImitation/Variant_TwinStick/UI"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
